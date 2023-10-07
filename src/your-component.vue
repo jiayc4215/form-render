ebfb6935bb22f6f44e3337dce0a01571b2954119
jiayc <2806242786@qq.com>
2023-10-07 09:17:15 +0800

    自定义组件完成

--- src/your-component.vue
+++ src/your-component.vue
@@ -1,21 +1,30 @@
 <template>
-  <el-input :modelValue="value" @input='onInput' v-bind="$attrs"></el-input>
-  {{ value }}
+  <el-input v-model="newValue" v-bind="$attrs"></el-input>
 </template>
 
 <script setup>
-let emit = defineEmits(["customEvent", 'update:modelValue']);
-import { watch } from 'vue';
+let emit = defineEmits(["customEvent", "update:modelValue"]);
+import { watch, computed } from "vue";
 let props = defineProps({
-  value: String,
-  title: String
-})
-watch(() => props.value, () => {
-  console.log('触发');
-  emit('customEvent', value,)
-})
-const onInput = (val) => {
-
-  emit('update:modelValue', val)
-}
+  modelValue: String,
+  title: String,
+});
+watch(
+  () => props.modelValue,
+  () => {
+    console.log("触发");
+    emit("customEvent", newValue.value);
+  }
+);
+// 计算属性求和
+const newValue = computed({
+  get: () => {
+    console.log("get");
+    return props.modelValue;
+  },
+  set: (value) => {
+    console.log("set");
+    return emit("update:modelValue", value);
+  },
+});
 </script>
