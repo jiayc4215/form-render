9cfc5388e620dd6df1c39875d6a6d202d39a6ff5
jiayc <2806242786@qq.com>
2023-10-27 15:01:58 +0800

    添加测试用例

--- src/view/label.vue
+++ src/view/label.vue
@@ -1,25 +1,29 @@
 <template>
-    <div class="box">slot-label</div>
-    <el-form-renderer label-width="100px" :content="content" inline />
+  <div class="box">slot-label</div>
+  <el-form-renderer label-width="100px" :content="content" inline />
 </template>
 <script setup>
 import { reactive, h, resolveComponent } from "vue";
 import elFormRenderer from "../components/femessage/el-form-renderer.vue";
 const content = reactive([
-    {
-        type: 'input',
-        id: 'name',
-        label: h('span', {}, '输入'),
-        el: {
-            size: 'small',
-        }
-
-    }
+  {
+    type: "input",
+    id: "name",
+    el: {
+      size: "small",
+    },
+    label: h("div", null, [
+      h("span", null, "编辑"),
+      h(resolveComponent("el-icon"), { class: "el-icon--right", size: 20 }, () => [
+        h(resolveComponent("Edit")),
+      ]),
+    ]),
+  },
 ]);
 </script>
 <style scoped>
 .box {
-    margin: 20px 0;
-    font-size: 20px;
+  margin: 20px 0;
+  font-size: 20px;
 }
 </style>
