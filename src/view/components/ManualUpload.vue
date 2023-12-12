1489f24ff1f42693180f5199d8d5d89712eeeaae
jiayc <2806242786@qq.com>
2023-12-12 18:14:15 +0800

    文件上传bug待解决

--- src/view/components/ManualUpload.vue
+++ src/view/components/ManualUpload.vue
@@ -23,6 +23,19 @@ auto-upload 是否自动上传文件
       ref="upload"
       :action="uploadFileUrl"
       :on-preview="handlePreview"
+      :name="name"
+      :limit="limit"
+      :data="{ ...extraData }"
+      :accept="fileType.join(',')"
+      :on-error="handleUploadError"
+      :on-exceed="handleExceed"
+      :on-success="handleUploadSuccess"
+      :before-upload="handleBeforeUpload"
+      :on-change="handleChange"
+      :on-remove="handleRemove"
+      :file-list="fileList"
+      :headers="headers"
+      :auto-upload="false"
     >
       <template #trigger>
         <el-button size="small" type="primary">选取文件</el-button>
@@ -45,14 +58,22 @@ auto-upload 是否自动上传文件
   </div>
 </template>
 <script setup>
-import { ref, computed, reactive, watch } from "vue";
+import { ref, computed, reactive, watch, nextTick, inject } from "vue";
+import { ElMessage, ElLoading } from "element-plus";
+let fileList = reactive([]); // 上传的文件列表
+let number = ref(0); // 准备上传的个数
+let extraData = reactive({}); // 上传的附加参数
+let headers = reactive({ Authorization: "Bearer " + 1111 }); // 设置上传的请求头部
+let loading = null;
+const upload = ref("");
+const emit = defineEmits(["update:modelValue"]);
 const props = defineProps({
   // 回显的值
   modelValue: [String, Object, Array],
   // 	必选参数，上传的地址
   action: {
     type: String,
-    default: "/dev-api/admin/product/fileUpload",
+    default: "/sys/user/import",
   },
 
   // 数量限制
@@ -85,6 +106,7 @@ const props = defineProps({
     default: "",
   },
 });
+const uploaded = inject("uploaded");
 // 提交地址
 const uploadFileUrl = computed(() => {
   return props.action;
@@ -92,4 +114,140 @@ const uploadFileUrl = computed(() => {
 const showTip = computed(() => {
   return props.isShowTip && (props.fileType || props.fileSize);
 });
+watch(
+  () => props.modelValue,
+  (val) => {
+    if (val) {
+      let temp = 1;
+      const list = Array.isArray(val) ? val : props.modelValue.split(",");
+      fileList = list.map((item) => {
+        if (typeof item === "string") {
+          item = { name: item, url: item };
+        }
+        // uid
+        item.uid = item.uid || new Date().getTime() + temp++;
+        return item;
+      });
+    } else {
+      fileList = [];
+      return [];
+    }
+  },
+  {
+    deep: true,
+    immediate: true,
+  }
+);
+// 上传前校检格式和大小
+const handleBeforeUpload = (file) => {
+  if (props.fileType) {
+    // 用来存储文件的扩展名
+    let fileExtension = "";
+    // 如果包含扩展名返回扩展名
+    if (file.name.lastIndexOf(".") > -1) {
+      fileExtension = file.name.slice(file.name.lastIndexOf(".") + 1);
+    }
+    // 使用some方法来判断文件类型是否匹配
+    const isTypeOk = props.fileType.some((type) => {
+      if (file.type.indexOf(type) > -1) return true;
+      // 如果文件的类型（file.type）中包含当前遍历到的类型（type），则返回true，表示文件类型匹配成功。
+      if (fileExtension && fileExtension.indexOf(type) > -1) return true;
+      return false;
+    });
+    //文件格式不正确
+    if (!isTypeOk) {
+      ElMessage({
+        message: `文件格式不正确, 请上传${props.fileType.join(
+          ","
+        )}图片格式文件!`,
+        type: "error",
+      });
+      return false;
+    }
+  }
+  // 会检查文件的大小是否小于指定的大小。
+  if (props.fileSize) {
+    const isLt = file.size / 1024 / 1024 < props.fileSize;
+    if (!isLt) {
+      ElMessage({
+        message: `上传文件大小不能超过 ${props.fileSize} MB!`,
+        type: "error",
+      });
+      return false;
+    }
+  }
+  loading = ElLoading.service({
+    lock: true,
+    text: "正在上传文件，请稍候...",
+    background: "rgba(0, 0, 0, 0.7)",
+  });
+  number.value++;
+};
+// 文件个数超出
+const handleExceed = () => {
+  ElMessage({
+    message: `上传文件数量不能超过 ${props.limit} 个!`,
+    type: "error",
+  });
+};
+// 上传失败
+const handleUploadError = (error) => {
+  uploaded(error);
+  ElMessage({
+    message: "上传图片失败，请重试",
+    type: "error",
+  });
+  loading.close();
+};
+// 提交上传(父组件控制上传)
+const submitUpload = async (extraData) => {
+  extraData = { ...extraData };
+  await nextTick();
+  upload.value.submit();
+};
+// 删除文件
+const handleRemove = (file, fileList) => {
+  emit("update:modelValue", fileList);
+};
+
+// 文件状态改变时的钩子
+const handleChange = (file, fileList) => {
+  emit("update:modelValue", fileList);
+};
+// 上传成功的回调
+const handleUploadSuccess = (res, file) => {
+  console.log("上传成功的回调");
+  if (res.code === 0) {
+    loading.close();
+    ElMessage({
+      message: "上传成功",
+      type: "success",
+    });
+  } else if (res.code === 401) {
+    ElMessage({
+      message: "登录状态过期，请重新登录",
+      type: "error",
+    });
+  } else {
+    // 删除上传
+    number.value--;
+    loading.close();
+    ElMessage({
+      message: "上传失败",
+      type: "error",
+    });
+    upload.value.handleRemove(file);
+  }
+  uploaded(res);
+};
+// 点击文件列表中已上传的文件时的钩子
+const handlePreview = (file, List) => {
+  console.log(file);
+};
+
+defineExpose({
+  submitUpload,
+});
 </script>
+
+
--- src/view/uploadFile.vue
+++ src/view/uploadFile.vue
@@ -1,20 +1,22 @@
 <template>
   <el-form-renderer ref="formRender" :content="content" />
+  <el-button size="small" type="primary" @click="submitForm">提交</el-button>
 </template>
 <script setup>
-import { reactive, markRaw, ref } from "vue";
+import { reactive, markRaw, ref, provide } from "vue";
 import elFormRenderer from "../components/femessage/el-form-renderer.vue";
 
 import ManualUpload from "./components/ManualUpload.vue";
 
 const formRender = ref("");
+
 const content = reactive([
   {
     label: "上传文件",
-    component: ManualUpload,
+    component: markRaw(ManualUpload),
     el: {
       limit: 1,
-      action: "/admin-api/control/data/import-merchant",
+      action: "aa",
     },
     id: "files",
     rules: [
@@ -27,18 +29,32 @@ const content = reactive([
     ],
   },
 ]);
-const getValue = () => {
-  const value = formRender.value.getFormValue();
 
-  console.log(value);
+const uploaded = (res) => {
+  if (res.code !== 500) {
+    console.log(res, "上传结果");
+  }
 };
+const submitForm = () => {
+  try {
+    formRender.value.methods.validate((valid) => {
+      if (valid) {
+        console.log("OK");
+        let params = formRender.value.getFormValue();
+        formRender.value.getComponentById("files").submitUpload(params);
 
-const setValue = () => {
-  formRender.value.updateForm({
-    identityBack:
-      "https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100,https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100",
-  });
+        //调用文件上传
+        console.log(params);
+      } else {
+        console.log("error submit!!");
+        return false;
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
+provide("uploaded", uploaded);
 </script>
 <style scoped>
 .box {
