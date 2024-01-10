b1b2f603b5fc9cb56470a00b61c6a9f0af0a113b
jiayc <2806242786@qq.com>
2024-01-10 09:47:14 +0800

    修复 group 无法使用css类名控制样式

--- src/components/femessage/components/render-form-group.vue
+++ src/components/femessage/components/render-form-group.vue
@@ -1,26 +1,24 @@
 <template>
-  <div>
-    <template v-for="(item, index) in data.items" :key="index">
-      <slot :name="`id:${item.id}`" />
-      <slot :name="`$id:${item.id}`" />
+  <template v-for="(item, index) in data.items" :key="index">
+    <slot :name="`id:${item.id}`" />
+    <slot :name="`$id:${item.id}`" />
 
-      <render-form-item
-        :ref="
-          (el) => {
-            customComponent[`formItem-${item.id}`] = el;
-          }
-        "
-        :prop="`${data.id}.${item.id}`"
-        :data="item"
-        :value="value"
-        :item-value="itemValue[item.id]"
-        :disabled="disabled"
-        :readonly="readonly"
-        :options="options[item.id]"
-        @updateValue="updateValue"
-      />
-    </template>
-  </div>
+    <render-form-item
+      :ref="
+        (el) => {
+          customComponent[`formItem-${item.id}`] = el;
+        }
+      "
+      :prop="`${data.id}.${item.id}`"
+      :data="item"
+      :value="value"
+      :item-value="itemValue[item.id]"
+      :disabled="disabled"
+      :readonly="readonly"
+      :options="options[item.id]"
+      @updateValue="updateValue"
+    />
+  </template>
 </template>
 <script setup>
 import RenderFormItem from "./render-form-item.vue";
--- src/view/getcomponent.vue
+++ src/view/getcomponent.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <el-form-renderer :content="content" ref="form"> </el-form-renderer>
+    <el-form-renderer :content="content" ref="form" class="render-form">
+    </el-form-renderer>
     <el-button @click="getComponent('id')">获取id input</el-button>
     <el-button @click="getComponent('first')">获取first name input</el-button>
   </div>
@@ -41,9 +42,70 @@ const getComponent = (id) => {
   console.log(form.value.getComponentById(id));
 };
 </script>
-<style scoped>
+<style scoped lang='scss'>
 .box {
   margin: 20px 0;
   font-size: 20px;
+}
+.render-form {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  flex-shrink: 0;
+  flex-grow: 0;
+  background: #fff;
+  margin: -20px -20px 20px;
+  padding: 20px 20px 0;
+  gap: 0 calc(4% / 4);
+
+  &.is-dialog .el-form-item__label {
+    min-width: 100px;
+    text-align: justify;
+    text-align-last: justify;
+  }
+
+  &.is-position-top {
+    > .el-form-item {
+      flex-wrap: wrap;
+
+      .el-form-item__label {
+        width: 100%;
+      }
+
+      .el-form-item__content {
+        flex: 1;
+        width: 100%;
+        margin-left: 0 !important;
+      }
+    }
+  }
+
+  // justify-content: space-between;
+  > .el-form-item {
+    width: 18%;
+    min-width: 300px;
+    display: flex;
+    flex-shrink: 0;
+    flex-grow: 0;
+
+    &:last-child {
+      width: auto;
+      // min-width: 180px;
+    }
+
+    &.date-range {
+      width: 440px;
+    }
+
+    .el-form-item__content {
+      flex: 1;
+      margin-left: 0 !important;
+
+      .el-date-editor--datetimerange.el-input,
+      .el-date-editor--datetimerange.el-input__inner {
+        width: 100%;
+      }
+    }
+  }
 }
 </style>
