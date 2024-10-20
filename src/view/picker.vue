30f718683d0562d44ea08400345adb13ec843bca
贾公子 <9681950+childe-jia@user.noreply.gitee.com>
2024-10-20 17:37:59 +0800

    文档修改

--- src/view/picker.vue
+++ src/view/picker.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="box">date-picker</div>
-  <el-form-renderer :content="content" />
+  <el-form-renderer ref="form" :content="content" />
+  <el-button type="primary" @click="printValue">print</el-button>
+  {{ data }}
 </template>
 <script setup>
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 import elFormRenderer from "../components/femessage/el-form-renderer.vue";
-
+const data = ref({});
+const form = ref();
 const content = reactive([
   {
     id: "date",
@@ -13,10 +16,13 @@ const content = reactive([
     label: "date",
     el: {
       // type: 'daterange',   // 如果 type 为 daterange，则获取到的是一个数组
-      valueFormat: "timestamp", // 不使用 value-format 属性，则获取到的是一个空对象，格式请参考 element-ui 组件 date-picker 的日期格式部分
+      valueFormat: "x", // 不使用 value-format 属性，则获取到的是一个空对象，格式请参考 element-ui 组件 date-picker 的日期格式部分
     },
   },
 ]);
+const printValue = () => {
+  data.value = form.value.getFormValue();
+};
 </script>
 <style scoped>
 .box {
