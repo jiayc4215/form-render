e7b25b3e6a4525766b314cbded58b988a204ef2a
jiayingchao <2806242786@qq.com>
2025-09-29 23:07:19 +0800

    feat(form): 添加表单字段类型支持并实现事件监听
    为表单渲染器添加对select、checkbox-group和radio-group类型的支持
    实现表单字段的change和input事件监听
    添加选项组件渲染逻辑，支持不同样式和值类型

--- src/view/basic.vue
+++ src/view/basic.vue
@@ -20,6 +20,11 @@ const content = [
     el: {
       placeholder: "请输入用户名",
     },
+    on: {
+      input: (val) => {
+        console.log(val);
+      },
+    },
     rules: [{ required: true, message: "请输入用户名", trigger: "blur" }],
   },
   {
@@ -30,6 +35,84 @@ const content = [
       placeholder: "请输入年龄",
     },
     rules: [{ required: true, message: "请输入年龄", trigger: "blur" }],
+  },
+  {
+    id: "gender",
+    type: "select",
+    label: "性别",
+    el: {
+      placeholder: "请选择性别",
+      valueKey: "id",
+    },
+    rules: [{ required: true, message: "请选择性别", trigger: "change" }],
+    on: {
+      change: (val) => {
+        console.log(val);
+      },
+    },
+    options: [
+      {
+        label: "男",
+        value: {
+          id: "male",
+          name: "male",
+        },
+      },
+      {
+        label: "女",
+        value: {
+          id: "female",
+          name: "female",
+        },
+      },
+    ],
+  },
+  {
+    id: "hobby",
+    type: "checkbox-group",
+    style: "button",
+    label: "爱好",
+    el: {
+      placeholder: "请选择爱好",
+    },
+
+    options: [
+      {
+        label: "篮球",
+        value: "basketball",
+      },
+      {
+        label: "足球",
+        value: "football",
+      },
+      {
+        label: "跑步",
+        value: "running",
+      },
+    ],
+  },
+  {
+    id: "interest",
+    type: "radio-group",
+
+    label: "兴趣",
+    el: {
+      placeholder: "请选择兴趣",
+    },
+    options: [
+      {
+        label: "运动",
+        value: "sport",
+      },
+      {
+        label: "阅读",
+        value: "reading",
+      },
+      {
+        label: "旅游",
+        value: "travel",
+      },
+    ],
   },
 ];
 const submit = () => {
--- src/view/test_demo/el-form-renderer.vue
+++ src/view/test_demo/el-form-renderer.vue
@@ -11,7 +11,56 @@
         :is="`el-${item.type}`"
         v-model="FormData[item.id]"
         v-bind="item.el"
-      />
+        v-on="item?.on || {}"
+      >
+        <template v-for="(option, index) in item.options">
+          <el-option
+            v-if="item.type === 'select'"
+            v-bind="option"
+            :key="getOptionKey(item, option) || index"
+          >
+          </el-option>
+          <el-checkbox-button
+            v-if="item.type === 'checkbox-group' && item.style === 'button'"
+            :key="option.value"
+            v-bind="option"
+            :label="'value' in option ? option.value : option.label"
+          >
+            {{ option.label }}
+          </el-checkbox-button>
+          <template
+            v-else-if="
+              item.type === 'checkbox-group' && item.style !== 'button'
+            "
+          >
+            <el-checkbox
+              :key="option.value"
+              v-bind="option"
+              :label="'value' in option ? option.value : option.label"
+            >
+              {{ option.label }}
+            </el-checkbox>
+          </template>
+
+          <el-radio-button
+            v-else-if="item.type === 'radio-group' && item.style === 'button'"
+            :key="option.label"
+            v-bind="option"
+            :label="'value' in option ? option.value : option.label"
+            >{{ option.label }}</el-radio-button
+          >
+          <template
+            v-else-if="item.type === 'radio-group' && item.style !== 'button'"
+          >
+            <el-radio
+              :key="option.label"
+              v-bind="option"
+              :label="'value' in option ? option.value : option.label"
+              >{{ option.label }}</el-radio
+            >
+          </template>
+        </template>
+      </component>
     </el-form-item>
   </el-form>
 </template>
@@ -30,6 +79,14 @@ const props = defineProps({
     default: () => {},
   },
 });
+const getOptionKey = (item, option) => {
+  if (option.value instanceof Object) {
+    if (!item?.el || !item?.el?.valueKey) return;
+    return option.value[item?.el?.valueKey];
+  } else {
+    return option.value;
+  }
+};
 const FormData = useVModle(props, "modelValue", emit);
 defineExpose({
   validate: (...args) => formRef.value.validate(...args),
