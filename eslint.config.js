f67e7c63536618d57d5776dbc8077c33fcb497cd
jiayingchao <2806242786@qq.com>
2026-01-16 22:46:44 +0800

    feat(root): 代码格式化

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,26 @@
-import js from "@eslint/js"; //js规范（标准的）
-import globals from "globals"; //环境
-import pluginVue from "eslint-plugin-vue"; //vue规范
-import { defineConfig } from "eslint/config"; //配置
-const ignores = ["**/dist/**", "**/node_modules/**", ".*"];
+import js from "@eslint/js" //js规范（标准的）
+import globals from "globals" //环境
+import pluginVue from "eslint-plugin-vue" //vue规范
+import { defineConfig } from "eslint/config" //配置
+import eslintConfigPrettier from "eslint-config-prettier" // prettier
+const ignores = ["**/dist/**", "**/node_modules/**", ".*"]
 export default defineConfig([
+  pluginVue.configs["flat/essential"], //vue规范
   {
-    files: ["**/src/**.{js,mjs,cjs,vue}"], //匹配文件
+    files: ["**/*.{js,mjs,cjs,vue}"], //匹配文件
     plugins: { js },
     extends: ["js/recommended"], //js规范
-    languageOptions: { globals: { ...globals.browser, ...globals.node } }, //全局变量 window
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }, //全局变量 window
     ignores, //忽略文件
-  },
-  pluginVue.configs["flat/essential"], //vue规范
-]);
+
+    ...eslintConfigPrettier,
+    rules: {
+      "vue/multi-word-component-names": "off"
+    }
+  }
+])
--- src/view/watch.vue
+++ src/view/watch.vue
@@ -1,28 +1,22 @@
 <template>
   <div>
     <h1>watch监听对象问题</h1>
-    <button @click="FormData.delivery = !FormData.delivery">
-      change delivery
-    </button>
+    <button @click="FormData.delivery = !FormData.delivery">change delivery</button>
     <button @click="changeRef">改变引用地址</button>
     <button @click="state.count++">改变state.count {{ state.count }}</button>
   </div>
 </template>
 
 <script setup>
-import _clonedeep from "lodash.clonedeep";
-import { ref, watch, computed, reactive, toValue } from "vue";
+import _clonedeep from "lodash.clonedeep"
+import { ref, watch, reactive, toValue } from "vue"
 const FormData = ref({
   delivery: false,
   type: [],
   resource: "A",
-  desc: "",
-});
-const computeData = computed(() => {
-  //   return Object.assign({}, FormData.value);
-  //   return _clonedeep(FormData.value);
-  return JSON.stringify(FormData.value);
-});
+  desc: ""
+})
+
 // watch(
 //   () => computeData.value,
 //   (newVal, oldVal) => {
@@ -42,48 +36,48 @@ const computeData = computed(() => {
 watch(
   () => FormData.value,
   (newVal, oldVal) => {
-    console.log("newVal", newVal);
-    console.log("oldVal", oldVal);
-    console.log("FormData change", newVal === oldVal);
-    console.log("FormData change", newVal.type === oldVal.type);
+    console.log("newVal", newVal)
+    console.log("oldVal", oldVal)
+    console.log("FormData change", newVal === oldVal)
+    console.log("FormData change", newVal.type === oldVal.type)
   },
   { deep: true }
-);
+)
 const changeRef = () => {
   FormData.value = {
-    ...FormData.value,
-  };
-};
-const cloneDeep = (obj) => {
-  return JSON.parse(JSON.stringify(obj));
-};
-const watchOldValue = (source, cb, options) => {
-  const { clone = cloneDeep } = options || {};
-  let val = toValue(source);
-  if (typeof val !== "object" || val === null) {
-    return watch(source, cb, options);
+    ...FormData.value
   }
-  let oldVal = clone(val);
+}
+const cloneDeep = obj => {
+  return JSON.parse(JSON.stringify(obj))
+}
+const watchOldValue = (source, cb, options) => {
+  const { clone = cloneDeep } = options || {}
+  let val = toValue(source)
+  if (typeof val !== "object" || val === null) {
+    return watch(source, cb, options)
+  }
+  let oldVal = clone(val)
   return watch(
     source,
     (newVal, _, onCleanup) => {
-      cb(newVal, oldVal, onCleanup);
-      oldVal = clone(newVal);
+      cb(newVal, oldVal, onCleanup)
+      oldVal = clone(newVal)
     },
     options
-  );
-};
+  )
+}
 const state = reactive({
   count: 1,
-  age: undefined,
-});
+  age: undefined
+})
 watchOldValue(
   () => state,
   (newVal, oldVal) => {
-    console.log("newVal", newVal);
-    console.log("oldVal", oldVal);
-    console.log("state change", newVal === oldVal);
+    console.log("newVal", newVal)
+    console.log("oldVal", oldVal)
+    console.log("state change", newVal === oldVal)
   },
   { deep: true, clone: _clonedeep }
-);
+)
 </script>
