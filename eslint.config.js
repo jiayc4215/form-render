c12853900a063e12b6e210e2fd4d88c250282412
jiayingchao <2806242786@qq.com>
2026-01-19 14:17:42 +0800

    feat(root): 初始化 e2e

--- eslint.config.js
+++ eslint.config.js
@@ -2,23 +2,31 @@ import js from "@eslint/js" //js规范（标准的）
 import globals from "globals" //环境
 import pluginVue from "eslint-plugin-vue" //vue规范
 import { defineConfig } from "eslint/config" //配置
+import pluginCypress from "eslint-plugin-cypress"
 import eslintConfigPrettier from "eslint-config-prettier" // prettier
 const ignores = ["**/dist/**", "**/node_modules/**", ".*", "**/.docs/**", "**/docs/.vitepress/**"]
 export default defineConfig([
+  // 1. 全局忽略
   {
     ignores
   },
+  // 2. 继承各个插件的推荐配置 (它们是独立的配置对象)
   pluginVue.configs["flat/essential"], //vue规范
+  pluginCypress.configs.recommended, //cypress规范
+
+  // 3. 自定义全局设置
   {
     files: ["**/*.{js,mjs,cjs,vue}"], //匹配文件
     plugins: { js },
     extends: ["js/recommended"], //js规范
+
+    //全局变量 window
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node
       }
-    }, //全局变量 window
+    },
     ...eslintConfigPrettier,
     rules: {
       "vue/multi-word-component-names": "off"
