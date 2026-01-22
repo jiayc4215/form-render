43f26c0eefcf5e4fab57286ee0efb18d00eba0bf
jiayingchao <2806242786@qq.com>
2026-01-22 21:48:11 +0800

    refactor(docs): docs文件重命名

--- src/_docs_/customFormRoules/MyInput.vue
+++ src/_docs_/customFormRoules/MyInput.vue
@@ -3,33 +3,33 @@
 </template>
 
 <script setup>
-let emit = defineEmits(["customEvent", "update:modelValue"]);
+let emit = defineEmits(["customEvent", "update:modelValue"])
 
-import { watch, computed, useAttrs } from "vue";
+import { watch, computed, useAttrs } from "vue"
 let props = defineProps({
   modelValue: String,
-  title: String,
-});
-const attrs = useAttrs();
-console.log(attrs);
+  title: String
+})
+const attrs = useAttrs()
+console.log(attrs)
 watch(
   () => props.modelValue,
   () => {
-    console.log("触发");
-    emit("customEvent", newValue.value);
+    console.log("触发")
+    emit("customEvent", newValue.value)
   }
-);
+)
 // 计算属性求和
 const newValue = computed({
   get: () => {
-    console.log("get");
-    return props.modelValue;
+    console.log("get")
+    return props.modelValue
   },
-  set: (value) => {
-    console.log("set");
-    return emit("update:modelValue", value);
-  },
-});
+  set: value => {
+    console.log("set")
+    return emit("update:modelValue", value)
+  }
+})
 </script>
 <script>
 export default {
@@ -43,13 +43,13 @@ export default {
 
   // rules 也可以是个函数, 参数是当前表单项配置, 需要返回一个数组.
   rules(item) {
-    console.log(item);
+    console.log(item)
     return [
       {
         required: true,
-        message: `${item.id} 不能为空`,
-      },
-    ];
-  },
-};
+        message: `${item.id} 不能为空`
+      }
+    ]
+  }
+}
 </script>
