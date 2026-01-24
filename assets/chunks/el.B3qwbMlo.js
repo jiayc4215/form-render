278f785a0470dfe9ee8f20e5c67d9820ae4ebc02
jiayc4215 <134461056+jiayc4215@users.noreply.github.com>
2026-01-24 08:49:50 +0000

    Deploying to gh-pages from @ jiayc4215/form-render@6f34510f8f2787dbb147b89c02e692fcbf6c1db9 🚀

---
+++ assets/chunks/el.B3qwbMlo.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.C2wUdEpc.js";import{b as n,o,k as a,a7 as c}from"./framework.Ca1m3vMN.js";const i={__name:"el",setup(r){const t=c([{id:"document",type:"input",el:{type:"textarea"}}]);return(s,_)=>(o(),n(a(e),{content:t},null,8,["content"]))}};export{i as _};
